4623abf17cbf05bd55d5ebfb4668cba8d28beefa
Dima Skibin <sixtynice77@gmail.com>
2019-04-15 13:44:18 +0300

    Distributed sections style added lasr blocks

---
+++ src/components/sections/sections.js
@@ -0,0 +1,50 @@
+const circlesCreative=document.querySelectorAll('.skills__circle-creative');
+const circlesTeamwork=document.querySelectorAll('.skills__circle-teamwork');
+const circlesInnovate=document.querySelectorAll('.skills__circle-innovate');
+const circlesCommunication=document.querySelectorAll('.skills__circle-communication');
+
+
+
+function addFillClass(count, name){
+    for(let i=0;i<count;i++) {
+        name[i].classList+=" fill";
+    }
+}
+
+
+function addNofillClass(count, name) {
+    
+
+    for(let i=name.length-count;i<12;i++) {
+        name[i].classList+=" nofill";
+    
+    }
+}
+
+
+addFillClass(9,circlesCreative);
+addNofillClass(3,circlesCreative);
+
+addFillClass(11,circlesTeamwork);
+addNofillClass(1,circlesTeamwork);
+
+addFillClass(6,circlesInnovate);
+addNofillClass(6,circlesInnovate);
+
+addFillClass(11,circlesCommunication);
+addNofillClass(1,circlesCommunication);
+
+
+
+
+
+
+
+
+
+
+
+
+function print(pa){
+    console.log(pa);
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import './main.scss';
+import './components/sections/sections'
